7a1db01aaec4c030a09f912d4f92c2c840f31a0c
tomeklarysz <ttomek.larysz@gmail.com>
2024-10-14 19:46:20 +0200

    finished two first exs

--- L01_matlab/zadania/z1_1.m
+++ L01_matlab/zadania/z1_1.m
@@ -2,4 +2,17 @@ clear all;
 close all;
 echo off;
 r = 3;   % promien
-v = 4/3 * pi * r,
+obwod = 2*pi*r,
+powierzchnia_kola = pi*r^2,
+
+powierzchnia_kuli = 4*pi*r^2,
+obj = 4/3 * pi * r^3,
+
+vkmh = 100;
+vms = vkmh*1000/3600,
+
+% w calach
+c = 55;
+x = 55/sqrt(16^2 + 9^2);
+szerokosc = 16*x,
+wysokosc = 9*x,
--- L01_matlab/zadania/z1_2.m
+++ L01_matlab/zadania/z1_2.m
@@ -1,11 +1,59 @@
 clear all; close all;
 a = 1; b = 5; c = 6;
+function y = quadratic(x,a,b,c)
+y = a*x.^2 + b*x + c;
+end
 p1 = (-b + sqrt(b^2 - 4*a*c))/2*a;
 p2 = (-b - sqrt(b^2 - 4*a*c))/2*a;
 
 disp("p1: " + p1);
 disp("p2: " + p2);
 
+function [x,y] = maksfun(a,b,c,start,ending)
+% only if quadratic
+if a > 0
+    s = quadratic(start,a,b,c);
+    e = quadratic(ending,a,b,c);
+    if s > e
+        y = s;
+        x = start;
+    else
+        y = e;
+        x = ending;
+    end
+    y = max(quadratic(start,a,b,c),quadratic(ending,a,b,c));
+else
+    p = -b/2*a;
+    x = p;
+    y = quadratic(p,a,b,c);
+end
+end
+
+function [x,y] = minfun(a,b,c,start,ending)
+% only if quadratic
+if a < 0
+    s = quadratic(start,a,b,c);
+    e = quadratic(ending,a,b,c);
+    if s < e
+        y = s;
+        x = start;
+    else
+        y = e;
+        x = ending;
+    end
+else
+    p = -b/2*a;
+    x = p;
+    y = quadratic(p,a,b,c);
+end
+end
+
 x = [-10:0.01:10];
-f = a*x.^2 + b*x + c;
-plot(x,f); grid on;
+f = quadratic(x,a,b,c);
+
+plot(x,f); grid on; hold on;
+[max_x,max_y] = maksfun(a,b,c,-10,10);
+[min_x,min_y] = minfun(a,b,c,-10,10);
+
+plot(max_x,max_y,"ro","MarkerSize",20);
+plot(min_x,min_y,"ro","MarkerSize",20);
