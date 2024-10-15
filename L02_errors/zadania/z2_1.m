e5c3a10dac99cc9ded76545235d5dd9f71987c9b
tomeklarysz <ttomek.larysz@gmail.com>
2024-10-15 14:40:14 +0200

    lab2 dwa pierwsze zadania

---
+++ L02_errors/zadania/z2_1.m
@@ -0,0 +1,9 @@
+a = fi(141,0,8,0); a.bin;
+b = fi(115,0,8,0); b.bin;
+c = fi(115,1,8,0); c.bin;
+d = fi(-115,1,8,0); d.bin;
+
+e = fi(17,0,8,0), e.bin,  % bez znaku 8b
+f = fi(17,0,16,0), f.bin, % bez znaku 16b
+g = fi(17,1,8,0), g.bin,  % ze znakiem 8b
+h = fi(17,1,16,0), h.bin, % ze znakiem 16b
